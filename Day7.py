2f69088dcbe238720755f7fbe9f80d2ed44779c2
Chí Sơn <caosychison3011@gmail.com>
2025-07-16 16:33:34 +0700

    16/07/2025 Med

---
+++ Day7.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+def nth_highest_salary(employee: pd.DataFrame, N: int) -> pd.DataFrame:
+    result = employee[['salary']].drop_duplicates().sort_values(by='salary',ascending=False)
+    if 0<N<= len(result):
+        return result.iloc[[N-1]].rename(columns={'salary':f'getNthHighestSalary({N})'})
+    return pd.DataFrame({f'getNthHighestSalary({N})': [np.nan]})
