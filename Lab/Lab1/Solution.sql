33f661c183f500a0b8ed6491601ea7c6ebe1e93e
Chí Sơn <caosychison3011@gmail.com>
2025-07-18 22:54:56 +0700

    Doing lab1

--- Lab/Lab1/Solution.sql
+++ Lab/Lab1/Solution.sql
@@ -1,5 +1,5 @@
 --1
-select region, count(customer_id) from sales group by region
+select region, count(id) from sales group by region
 
 --2 
 select product, sum(quantity) from sales group by product
@@ -8,4 +8,26 @@ select product, sum(quantity) from sales group by product
 select product, sum(quantity*price) from sales group by product
 
 --4
-select region, 
+select region, avg(quantity*price)from sales group by region
+
+--5
+select  count(distinct customer_id), product from sales group by product
+
+--6 
+select region from sales group by region order by sum(quantity*price) desc  limit 1
+
+--7 
+select  sale_date,product, sum(quantity*price) from sales group by product, sales_date
+
+--8 
+select customer_id from sales group by customer_id order by sum(quantity) desc  limit 1
+
+--9
+select region, product, revenue
+from (
+    select region, product, sum(quantity * price) as revenue,
+           row_number() over (partition by region order by sum(quantity * price) desc) as rn
+    from sales
+    group by region, product
+) t
+where rn = 1;
